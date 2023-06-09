d649ca2f72c2af1c94f69d85ef4143f6d13e4acb
hadh93 <hoondog1993@gmail.com>
2023-06-09 23:57:25 +0900

    1st problem added

---
+++ D4_Sewoon.c
@@ -0,0 +1,19 @@
+//
+// Created by hoond on 2023-06-09.
+//
+
+#include <stdio.h>
+
+int main(){
+    int n;
+    int num_testcases;
+    scanf_s("%d", &n);
+    for (int i=0; i<n; i++){
+        scanf_s("%d", &num_testcases);
+        for (int j=0; j<num_testcases; j++){
+
+        }
+        printf("%d", i);
+    }
+    return 0;
+}
